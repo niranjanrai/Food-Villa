b669d1562f4a18253aba663e312ce8513444e6c1
niranjanrai <rainiranjan1998@gmail.com>
2023-02-08 22:59:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+const div = React.createElement("h1", {}, "hello world");
+const root = React.createRoot(document.getElementById("root"));
+root.render(div);
+
+/**
+ * Bundlers : vite, webpack, parcel
+ * Parcel
+ *
+ *
+ *
+ *
+ */
